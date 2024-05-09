c346aefa87d1c4892ffcc77980f007cfc54a9da4
Mihaela Ivanova <mivanova@lelypartners.com>
2024-05-09 11:37:30 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using SignalRPresentation.Components;
+using SignalRPresentation.Hubs;
+
+namespace SignalRPresentation;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        builder.Services.AddResponseCompression(opts =>
+        {
+            opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                new[] { "application/octet-stream" });
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseResponseCompression();
+
+        app.UseRouting();
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.MapHub<ChatHub>("/chathub");
+
+        app.Run();
+    }
+}
